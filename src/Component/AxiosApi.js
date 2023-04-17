16782b4e9a2e96f184f69e13dea0a4133ae0b467
linux <narendrac024@gmail.com>
2023-04-17 15:52:00 +0530

    banner

--- src/Component/AxiosApi.js
+++ src/Component/AxiosApi.js
@@ -8,7 +8,8 @@ export const BannerApi = () => {
   useEffect(() => {
     const fetchData = async () => {
       const res= await axios.get(`${url}/movie`);
-      setBanner(res.data.slice(0,10) );
+      const randomIndex = Math.floor(Math.random() * res.data.length);
+      setBanner(res.data.slice(randomIndex, randomIndex + 10));
     };
     fetchData();
   }, []);
@@ -19,14 +20,59 @@ export const BannerTvShowApi = () => {
   const [Banner, setBanner] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const { data: { results } } = await axios.get(`${url}/tvShow`);
-      setBanner(results);
+      const { data } = await axios.get(`${url}/tvShow`);
+      const randomIndex = Math.floor(Math.random() * data.length);
+      setBanner(data.slice(randomIndex, randomIndex + 10));
     };
     fetchData();
   }, []);
   return Banner;
 };
 
+export const HomeBannerApi = () => {
+  const [bannerData, setBannerData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const moviesRes = await axios.get(`${url}/movie`);
+      const tvShowsRes = await axios.get(`${url}/tvShow`);
+
+      const moviesData = moviesRes.data;
+      const tvShowsData = tvShowsRes.data;
+
+      // Combine the movie and TV show data
+      const combinedData = [...moviesData, ...tvShowsData];
+
+      // Shuffle the combined data array
+      for (let i = combinedData.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [combinedData[i], combinedData[j]] = [combinedData[j], combinedData[i]];
+      }
+
+      // Slice the shuffled array to get the desired number of items
+      const slicedData = combinedData.slice(0, 10);
+
+      setBannerData(slicedData);
+    };
+
+    fetchData();
+  }, []);
+
+  return bannerData;
+};
+
+
+export const BannerAnimeApi = () => {
+  const [Banner, setBanner] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`${url}/anime`);
+      setBanner(data.slice(0,10));
+    };
+    fetchData();
+  }, []);
+  return Banner;
+};
 export const MovieApi=(TypeMovies)=>{
 	const [Movies, setMovies] = useState([]);
 		useEffect(()=>{
--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,47 +1,66 @@
-import React, { useState,useEffect } from 'react';
-
-import "./Banner.scss"
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState, useEffect } from 'react';
+import './Banner.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
-import { BannerApi } from '../AxiosApi';
-import { Link } from "react-router-dom";
+import { BannerApi, BannerTvShowApi, HomeBannerApi } from '../AxiosApi';
+import { Link, useLocation } from 'react-router-dom';
+
 function Banner() {
+  const location = useLocation(); // get the current route
 
-const BannerType=BannerApi();
-console.log(BannerType[0]);
- return (
-    <> 
-    <div className='poster'>
-      <Carousel
-      showThumbs={false}
-      autoPlay={true}
-      transitionTime={3}
-infiniteLoop={true}
-showStatus={false}
->{
-    BannerType.map(movie=>(  <Link to={'/MovieMain/'} state={{ tvShow: movie }} style={{textDecoration:"none", color:"white"}}>
-    <div className="posterImage">
-        <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
-    </div>
-    <div className="posterImage__overlay">
-        <div className="posterImage__title">{movie ? movie.title: ""}</div>
-        <div className="posterImage__runtime">
-            {movie ? movie.release_date : ""}
-            <span className="posterImage__rating">
-                {movie ? movie.vote_average :""}
-                <i className="fas fa-star" />{" "}
-            </span>
-        </div>
-        <div className="posterImage__description">{movie ? movie.overview : ""}</div>
-    </div>
-</Link>
-))
-}
-</Carousel>
-    </div>
-    
+  let bannerData = [];
+
+  if (location.pathname === '/TvShows') {
+    bannerData = BannerTvShowApi();
+  } else if (location.pathname === '/') {
+    bannerData = HomeBannerApi();
+  } else {
+    bannerData = BannerApi();
+  }
+
+  return (
+    <>
+      <div className='poster'>
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {bannerData.map((movie) => (
+            <Link
+              to={{ pathname: '/MovieMain/', state: { tvShow: movie } }}
+              key={movie.id}
+              style={{ textDecoration: 'none', color: 'white' }}
+            >
+              <div className='posterImage'>
+                <img
+                  src={`${movie && movie.backdrop_path}`}
+                  alt={movie ? movie.name || movie.title : ''}
+                />
+              </div>
+              <div className='posterImage__overlay'>
+                <div className='posterImage__title'>
+                  {movie ? movie.name || movie.title : ''}
+                </div>
+                <div className='posterImage__runtime'>
+                  {movie ? movie.release_date : ''}
+                  <span className='posterImage__rating'>
+                    {movie ? movie.vote_average : ''}
+                    <i className='fas fa-star' />{' '}
+                  </span>
+                </div>
+                <div className='posterImage__description'>
+                  {movie ? movie.overview : ''}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
+      </div>
     </>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
